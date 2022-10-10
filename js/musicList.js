c0600c81d3ffd17afce04f464bae2c592ec8baa8
ZeZ17 <89334679+ZeZ17@users.noreply.github.com>
2022-10-10 22:16:06 +0800

    music player

---
+++ js/musicList.js
@@ -0,0 +1,22 @@
+let allMusic = [
+    {
+        name: "Dark",
+        artist: "Rave",
+        img: "music1",
+        src: "music1"
+    },
+
+    {
+        name: "Amend",
+        artist: "jpn",
+        img: "music2",
+        src: "music2",
+    },
+    
+    {
+        name: "Never Say Never",
+        artist: "Three Lights Down Kings",
+        img: "music3",
+        src: "music3",
+    }
+]
